f67e2fac9b3cd4fbcc9b37318669052d2f9a9cde
Pragatheswari Avuku <pragatheswari2004@gmail.com>
2023-05-10 11:04:23 +0530

    10-05-2023

---
+++ Remove_consective_Characters.py
@@ -0,0 +1,17 @@
+#User function Template for python3
+
+class Solution:
+    def removeConsecutiveCharacter(self, S):
+        i=0
+        new=''
+        if len(s)<2:
+            return S
+        while i<len(S)-1:
+            char=S[i]
+            new+=char
+            while S[i]==char and i<len(S)-1:
+                i+=1
+        if new[-1]!=S[-1]:
+            new+=S[-1]
+        return new
+#https://practice.geeksforgeeks.org/problems/consecutive-elements2306/1?page=1&difficulty[]=-1&status[]=unsolved&category[]=Strings&sortBy=submissions  
