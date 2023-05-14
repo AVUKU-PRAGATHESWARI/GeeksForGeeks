3ee9ec20490126d5b89d926eca8ddd52b43f10b1
Pragatheswari Avuku <pragatheswari2004@gmail.com>
2023-05-14 19:04:11 +0530

    14-05-2023

---
+++ Print_first_letter_of_every_word_in_the_string.py
@@ -0,0 +1,9 @@
+class Solution:
+	def firstAlphabet(self, S):
+	    string=''
+	    for i in S.split():
+	        string+=i[0]
+        return string
+
+
+# https://practice.geeksforgeeks.org/problems/print-first-letter-of-every-word-in-the-string3632/1?page=1&difficulty[]=-1&status[]=unsolved&category[]=Strings&sortBy=submissions
