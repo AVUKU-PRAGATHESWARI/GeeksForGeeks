9cc97a115a753a19a9844c6792775be4848e7e17
Pragatheswari Avuku <pragatheswari2004@gmail.com>
2023-05-29 11:43:15 +0530

    29-05-2023

---
+++ Multiply left and right array sum..py
@@ -0,0 +1,9 @@
+#User function Template for python3
+
+def multiply (arr, n) : 
+    #Complete the function
+    sum1=sum(arr[:n//2])
+    sum2=sum(arr[n//2:])
+    return sum1*sum2
+
+# https://practice.geeksforgeeks.org/problems/multiply-left-and-right-array-sum1555/1?page=1&difficulty[]=-2&difficulty[]=-1&status[]=unsolved&category[]=Arrays&sortBy=submissions
