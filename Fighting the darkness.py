1396f12a9dc4c7cd6e481222378c2a9dd4c7777d
Pragatheswari Avuku <pragatheswari2004@gmail.com>
2023-06-03 05:35:35 +0530

    03-06-2023

---
+++ Fighting the darkness.py
@@ -0,0 +1,4 @@
+class Solution:
+    def maxDays(self, arr, n):
+        return max(arr)
+# https://practice.geeksforgeeks.org/problems/fighting-the-darkness3949/1?page=1&difficulty[]=-1&status[]=unsolved&category[]=Arrays&sortBy=submissions
