11532ebc957373daba0cfc199ce7000882390114
Pragatheswari Avuku <pragatheswari2004@gmail.com>
2023-06-18 22:55:49 +0530

    18-06-2023

---
+++ Sum of distinct elements.py
@@ -0,0 +1,7 @@
+#User function Template for python3
+class Solution:
+	
+	def findSum(self,arr, n):
+		new=list(set(arr))
+		return sum(new)
+# https://practice.geeksforgeeks.org/problems/sum-of-distinct-elements4801/1?page=2&difficulty[]=-1&status[]=unsolved&category[]=Arrays&sortBy=submissions
