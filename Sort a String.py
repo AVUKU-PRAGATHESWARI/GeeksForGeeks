9e49772610a7151b554046feb2070d06806ef054
Pragatheswari Avuku <pragatheswari2004@gmail.com>
2023-06-20 08:01:59 +0530

    20-06-2023

---
+++ Sort a String.py
@@ -0,0 +1,9 @@
+#User function Template for python3
+class Solution:
+    def sort(self, s): 
+        #code here
+        s=list(s)
+        s.sort()
+        return "".join(s)
+    
+# https://practice.geeksforgeeks.org/problems/sort-a-string2943/1?page=1&difficulty[]=-1&status[]=unsolved&category[]=Strings&sortBy=submissions
