435dd73fcb5dbe55a97e1670a0dbfb6bec6d3376
Pragatheswari Avuku <pragatheswari2004@gmail.com>
2023-06-30 14:24:23 +0530

    30-06-2023

---
+++ Is Binary Number Multiple of 3.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+class Solution:
+	def isDivisible(self, s):
+	    dec=int(s,2)
+	    if int(dec)%3==0:
+		    return 1
+		else:
+		    return 0
+
+# https://practice.geeksforgeeks.org/problems/is-binary-number-multiple-of-30654/1
