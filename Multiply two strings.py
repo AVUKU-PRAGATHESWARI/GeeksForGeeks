7a8d5a62772bec92a65a2cefaf4a39b109163b55
Pragatheswari Avuku <pragatheswari2004@gmail.com>
2023-07-21 08:08:31 +0530

    21-07-2023

---
+++ Multiply two strings.py
@@ -0,0 +1,5 @@
+class Solution:
+    def multiplyStrings(self,s1,s2):
+        return int(s1)*int(s2)
+    
+# https://practice.geeksforgeeks.org/problems/multiply-two-strings/1?page=1&status[]=unsolved&category[]=Strings&sortBy=submissions
