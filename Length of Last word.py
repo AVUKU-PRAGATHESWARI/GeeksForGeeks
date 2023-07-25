735567552aa27f6355849290ae9256830128a561
Pragatheswari Avuku <pragatheswari2004@gmail.com>
2023-07-25 07:50:01 +0530

    25-07-2023

---
+++ Length of Last word.py
@@ -0,0 +1,7 @@
+class Solution:
+
+    def findLength(self, s):
+        a=s.split()
+        last=a[-1]
+        return len(last)
+# https://practice.geeksforgeeks.org/problems/length-of-last-word5721/1?page=2&difficulty[]=-1&status[]=unsolved&category[]=Strings&sortBy=submissions
