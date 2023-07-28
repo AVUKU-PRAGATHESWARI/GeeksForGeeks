f62f67a15bc673cd6dda9e09921c51b1e68f1f23
Pragatheswari Avuku <pragatheswari2004@gmail.com>
2023-07-28 22:37:41 +0530

    28-07-2023

---
+++ Check if divisible by 11.py
@@ -0,0 +1,8 @@
+class Solution:
+	def divisibleBy11(self, S):
+		# Your Code Here
+		if not(int(S)%11):
+		    return 1
+		return 0
+	
+# https://practice.geeksforgeeks.org/problems/check-if-divisible-by-114724/1?page=2&difficulty[]=-1&status[]=unsolved&category[]=Strings&sortBy=submissions
