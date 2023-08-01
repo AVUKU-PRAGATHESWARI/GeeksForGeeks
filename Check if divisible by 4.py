cef27f52ed5e7bee1709e4bc4e9b020d8fd36d13
Pragatheswari Avuku <pragatheswari2004@gmail.com>
2023-08-01 08:47:10 +0530

    01-08-2023

---
+++ Check if divisible by 4.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+class Solution:
+	def divisibleBy4 (self, N):
+		# Your Code Here
+	    if int(N)%4==0:
+	        return 1
+	    return 0
+
+
+# https://practice.geeksforgeeks.org/problems/check-if-divisible-by-43813/1?page=1&difficulty[]=-1&status[]=unsolved&category[]=Strings&sortBy=submissions
