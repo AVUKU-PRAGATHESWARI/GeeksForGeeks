c7b43dbc1cdb69d99d2f1a28baf8c5dd56c2cbbd
Pragatheswari Avuku <pragatheswari2004@gmail.com>
2023-08-03 07:58:38 +0530

    03-08-2023

---
+++ Snake Case.py
@@ -0,0 +1,7 @@
+class Solution:
+    def snakeCase(self, S , n):
+        S=S.lower()
+        S=S.replace(' ','_')
+        return S
+    
+# https://practice.geeksforgeeks.org/problems/snake-case0219/1?page=1&difficulty[]=-1&status[]=unsolved&category[]=Strings&sortBy=submissions
