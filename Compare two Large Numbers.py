2c1083158500863ef71f9a13cafc923d5faed740
Pragatheswari Avuku <pragatheswari2004@gmail.com>
2023-08-10 07:55:10 +0530

    10-08-2023

---
+++ Compare two Large Numbers.py
@@ -0,0 +1,12 @@
+class Solution:
+
+    def check(self, a,b):
+        if a<b:
+            return 1
+        elif a>b:
+            return 2
+        else:
+            return 3
+
+
+# https://practice.geeksforgeeks.org/problems/compare-two-large-numbers3413/1?page=1&difficulty[]=-1&status[]=unsolved&category[]=Strings&sortBy=submissions
