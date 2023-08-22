2da66443f52a9dec6d7fb28858769f96ba9e06e1
Pragatheswari Avuku <pragatheswari2004@gmail.com>
2023-08-22 09:18:40 +0530

    22-08-2023

---
+++ Count the Zeros.py
@@ -0,0 +1,7 @@
+#User function Template for python3
+
+class Solution:
+    def countZeroes(self, arr, n):
+        return arr.count(0)
+
+# https://practice.geeksforgeeks.org/problems/count-the-zeros2550/1?page=2&status[]=unsolved&category[]=Arrays&sortBy=submissions
