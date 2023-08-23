064db0421db32701449b27130a3e7b89c71b57e1
Pragatheswari Avuku <pragatheswari2004@gmail.com>
2023-08-23 07:55:28 +0530

    23-08-2023

---
+++ 12 hour clock Multiplication.py
@@ -0,0 +1,6 @@
+
+class Solution:
+    def mulClock(self, num1, num2):
+        result = num1*num2
+        return result%12
+# https://practice.geeksforgeeks.org/problems/12-hour-clock-multiplication4709/1?page=1&difficulty[]=-2&status[]=unsolved&sortBy=submissions
